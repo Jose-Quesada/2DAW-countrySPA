a6565edb27b29207a93988b1adca0c40175b455e
Jose Quesada <jose.quesada@fernando3martos.com>
2024-11-19 12:02:12 +0100

    solo creado sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SharedSidebarComponent } from './components/shared-sidebar/shared-sidebar.component';
-import { SidebarComponent } from '../../../../gifs-app/src/app/shared/components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
@@ -11,10 +12,12 @@ import { SidebarComponent } from '../../../../gifs-app/src/app/shared/components
   declarations: [
     AboutPageComponent,
     HomePageComponent,
-    SharedSidebarComponent
+    SharedSidebarComponent,
+    ContactPageComponent
   ],
   imports: [
     CommonModule,
+    RouterModule
   ],
   exports: [
     AboutPageComponent,
